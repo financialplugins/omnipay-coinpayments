a26ff2815ec1a70ba857c6220a5eecf82c28ddb7
Alexey Eremenko <alex.eremenko@yahoo.com>
2020-02-04 21:16:31 +0300

    Coinpayments gateway implementation in progress

--- src/Message/RatesRequest.php
+++ src/Message/RatesRequest.php
@@ -5,8 +5,6 @@ namespace Omnipay\Coinpayments\Message;
 
 class RatesRequest extends AbstractRequest
 {
-    protected $responseClass = RatesResponse::class;
-    
     protected function getCommand(): string
     {
         return 'rates';
